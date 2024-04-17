143b29c93a6ac2204058b3afd14ad34bd62ab7af
kisspeter18 <kisspeti2000@gmail.com>
2024-04-17 21:03:40 +0200

    Try other integrate method in likelihood calc and vectorizing parameter estimation to speed up MLE

--- src/sucess_probability.py
+++ src/sucess_probability.py
@@ -15,8 +15,20 @@ def p_g(x_g, w_g, gamma_g):
         float or ndarray: Result of p_g(X_g).
     """
     # Ensure w_g and gamma_g are arrays for element-wise operations
-    w_g = np.asarray(w_g)
-    gamma_g = np.asarray(gamma_g)
+    w_g = np.asarray([w_g]) if isinstance(w_g, float) else np.asarray(w_g)
+    gamma_g = np.asarray([gamma_g]) if isinstance(gamma_g, float) else np.asarray(gamma_g)
+
+    x_g = np.asarray([x_g]) if isinstance(x_g, float) else np.asarray(x_g)
+
+    x_dim = len(x_g)
+    w_dim = len(w_g)
+
+    # Make w_g a x_dim x w_dim matrix where each row is the same
+    w_g = np.tile(w_g, (x_dim, 1))
+    # Make gamma_g a x_dim x w_dim matrix where each row is the same
+    gamma_g = np.tile(gamma_g, (x_dim, 1))
+    # Make x_g a x_dim x w_dim matrix where each column is the same
+    x_g = np.tile(x_g, (w_dim, 1)).T
 
     result = norm.cdf((gamma_g - w_g * x_g) / np.sqrt(1 - w_g**2))
 
