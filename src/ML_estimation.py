03cd1399c814988c9bd94d682a54768937ee5512
kisspeter18 <kisspeti2000@gmail.com>
2024-04-19 21:40:27 +0200

    Add MLE with trapz integration

--- src/ML_estimation.py
+++ src/ML_estimation.py
@@ -327,3 +327,126 @@ def ml_estimation_linear_with_w_and_g(
         gamma_result = np.array(gamma_calc_func(result.x, b_init))
 
     return factor_loading_result, gamma_result, result
+
+def calc_linear_likelihood(d_g_arr, n_g_arr, p_g, prob_dens_func, a, b):
+    y_values = np.linspace(0, 1, num=1000)
+
+    y_dim = len(y_values)
+    a_dim = len(a)
+
+    a_mat = np.tile(a, (y_dim, 1))
+    b_mat = np.tile(b, (y_dim, 1))
+    y_mat = np.tile(y_values, (a_dim, 1)).T
+
+    integrand_values = np.prod(binom.pmf(d_g_arr, n_g_arr, norm.cdf(a_mat * norm.ppf(y_mat) + b_mat)), axis=1)
+    result = cumtrapz(integrand_values, y_values)[-1]
+    return result
+
+
+def log_likehood_variable_changed_fast(d_g_array, n_g_array, p_g, prob_dens_func, a, b):
+    return sum(np.log(calc_linear_likelihood(d_g_list, n_g_list, p_g, prob_dens_func, a, b)) for d_g_list, n_g_list in zip(d_g_array, n_g_array))
+
+
+def mle_linear_trapz(default_table, num_of_obligors_table, a_init, b_init, fixed_a=False):
+    a_init = np.asarray([a_init]) if isinstance(a_init, float) else np.asarray(a_init)
+    b_init = np.asarray([b_init]) if isinstance(b_init, float) else np.asarray(b_init)
+
+    if not fixed_a:
+        initial_guess = np.concatenate((a_init, b_init))
+
+        num_of_a = len(a_init)
+        bounds = [(-10, 10)] * len(initial_guess)
+
+        # Optimization
+        objective_function = lambda params: -log_likehood_variable_changed_fast(
+            default_table, num_of_obligors_table, p_g, norm.pdf, params[:num_of_a], params[num_of_a:len(initial_guess)]
+        )
+
+        result = minimize(objective_function,
+                          initial_guess,
+                          method="Nelder-Mead",
+                          bounds=bounds,
+                          options={
+                              'disp': False})
+
+    else:
+        initial_guess = b_init
+
+        bounds = [(-10, 10)] * len(initial_guess)
+
+        # Optimization
+        objective_function = lambda params: -log_likehood_variable_changed_fast(
+            default_table, num_of_obligors_table, p_g, norm.pdf, a_init, params
+        )
+
+        result = minimize(objective_function,
+                          initial_guess,
+                          method="Nelder-Mead",
+                          bounds=bounds,
+                          options={
+                              'disp': False})
+
+    return result
+
+def mle_trapz_g_and_w(
+        default_table, num_of_obligors_table, factor_loading_init, gamma_list_init, fixed_w=False, fixed_g=False):
+    # if len(factor_loading_init) == 1:
+    #     factor_loading_init = np.full_like(gamma_list_init, factor_loading_init[0])
+    # elif len(gamma_list_init) == 1:
+    #     gamma_list_init = gamma_list_init * len(factor_loading_init)
+
+    a_init = np.array(a_calc_func(np.array(factor_loading_init), np.array(gamma_list_init)))
+    b_init = np.array(b_calc_func(np.array(factor_loading_init), np.array(gamma_list_init)))
+
+    initial_guess = np.concatenate((a_init, b_init))
+
+    num_of_a = len(a_init)
+    bounds = [(-10, 10)] * len(initial_guess)
+
+    # Optimization
+    if not fixed_w and not fixed_g:
+        objective_function = lambda params: -log_likehood_variable_changed_fast(
+            default_table, num_of_obligors_table, p_g, norm.pdf, params[:num_of_a], params[num_of_a:len(initial_guess)]
+        )
+
+        result = minimize(objective_function,
+                          initial_guess,
+                          method="Nelder-Mead",
+                          bounds=bounds,
+                          options={
+                              'disp': False})
+
+        factor_loading_result = np.array(w_calc_func(np.array(result.x[:num_of_a]), np.array(result.x[num_of_a:])))
+        gamma_result = np.array(gamma_calc_func(np.array(result.x[:num_of_a]), np.array(result.x[num_of_a:])))
+
+    elif fixed_w:
+        objective_function = lambda params: -log_likehood_variable_changed_fast(
+            default_table, num_of_obligors_table, p_g, norm.pdf, a_init, params
+        )
+
+        result = minimize(objective_function,
+                          b_init,
+                          method="Nelder-Mead",
+                          bounds=bounds[num_of_a:],
+                          options={
+                              'disp': False})
+
+        factor_loading_result = np.array(w_calc_func(a_init, result.x))
+        gamma_result = np.array(gamma_calc_func(a_init, result.x))
+
+    elif fixed_g:
+        objective_function = lambda params: -log_likehood_variable_changed_fast(
+            default_table, num_of_obligors_table, p_g, norm.pdf, params, b_init
+        )
+
+        result = minimize(objective_function,
+                          a_init,
+                          method="Nelder-Mead",
+                          bounds=bounds[:num_of_a],
+                          options={
+                              'disp': False})
+
+        factor_loading_result = np.array(w_calc_func(result.x, b_init))
+        gamma_result = np.array(gamma_calc_func(result.x, b_init))
+
+    return factor_loading_result, gamma_result, result
--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ from src.sucess_probability import p_g
 from src.ML_estimation import calculate_my_likelihood
 from src.ML_estimation import calculate_likelihood_ts
 from src.ML_estimation import maximum_likelihood_estimation
-from src.ML_estimation import multivariate_ml_estimation
+from src.ML_estimation import multivariate_ml_estimation, mle_trapz_g_and_w
 from src.MM_estimation import calc_variance_of_default_rate
 from src.MM_estimation import estimate_w_factor_loading
 from src.MM_estimation import gen_data_and_mm
