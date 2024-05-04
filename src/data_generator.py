6ab434bb4fed5a7fe199514209fc2953a7585c26
kisspeter18 <kisspeti2000@gmail.com>
2024-05-04 20:46:25 +0200

    Refactor data_generator

--- src/data_generator.py
+++ src/data_generator.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 def generate_y(factor_loading, num_of_obligors):
+    # Generate normalized return on obligors’ assets (Y) for only 1 bucket
     x = np.random.normal()
     epsilon = np.random.normal(0, 1, num_of_obligors)
     y = factor_loading * x + epsilon * (1 - factor_loading**2)**0.5
@@ -10,12 +11,14 @@ def generate_y(factor_loading, num_of_obligors):
 
 
 def generate_default(num_of_obligors, factor_loading, gamma):
+    # Generate num_of_defaults for only 1 bucket and 1 time point
     y = generate_y(factor_loading, num_of_obligors)
     default = (y < gamma).sum()
     return default
 
 
 def generate_default_buckets(factor_loading_list, num_of_obligors_list, gamma_list, time_points=160):
+    # Generate num_of_defaults for more grades and sum them up during time_points
     x = np.random.normal(0, 1, time_points)
     defaults_list = []
 
@@ -31,8 +34,9 @@ def generate_default_buckets(factor_loading_list, num_of_obligors_list, gamma_li
 
 
 def generate_default_time_series(factor_loading_list, num_of_obligors_list, gamma_list, time_points=160):
-    # if factor_loading_list's length is 1, then make it a list of the same length as num_of_obligors_list
+    # Generate time series of defaults for more grades
     if len(factor_loading_list) == 1:
+        # if factor_loading_list's length is 1, then make it a list of the same length as num_of_obligors_list
         factor_loading_list = [factor_loading_list[0]] * len(num_of_obligors_list)
     x = np.random.normal(0, 1, time_points)
     defaults_df = pd.DataFrame()
